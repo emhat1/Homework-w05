3672806bac4a2c36ae92823b43cda24cf935ce99
Erin H <e.hatherell@gmail.com>
2021-12-22 11:49:13 +1100

    Setting out tasks the scheduler needs to complete to be functional

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //WorkDay Scheduler
+
+
+//Display the current date and time
+
+
+//Assess whether each timeslot is past, present of future
+
+
+//Load data from local storage
+
+
+//Save data to local storage when button clicked
+
+
+
+
+
