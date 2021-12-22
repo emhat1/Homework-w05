cf9e44a77348bf2b0029852360fc33f4353d9d97
Erin H <e.hatherell@gmail.com>
2021-12-22 14:28:22 +1100

    Attempting to compare times between rows ... something still isn't right

--- script.js
+++ script.js
@@ -1,15 +1,26 @@
-//WorkDay Scheduler
-
 
 //Display the current date and time
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY , h:mm a"));
 $("currentTime").text(moment().format("h:mm a"));
 
+//Compare current time and row time and adjust tense as necessary
 
-//Assess whether each timeslot is past, present of future and changerow colour accordingly
+$(".time-block").each(function () {
+    var presentTime = moment().hour();
+    var blockTime = parseInt($(this).attr("id").split("time")[1]);
+    if (blockTime < presentTime) {
+      $(this).addClass("past");
+    } 
+    else if (blockTime == presentTime) {
+      $(this).removeClass("past").addClass("present");
+    } 
+    else {
+      $(this).removeClass("past", "present").addClass("future");
+    }
+  });
 
 
-//Load data from local storage
+  //Load data from local storage
 
 
 //Save data to local storage when button clicked
