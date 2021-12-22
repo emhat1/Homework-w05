ae074f23bee9d7d1d4bea3a5b567d765bc2393c0
Erin H <e.hatherell@gmail.com>
2021-12-22 14:49:02 +1100

    Unsuccessfully trying to save and retrieve entries from local storage

--- script.js
+++ script.js
@@ -3,8 +3,18 @@
 $("#currentDay").text(moment().format("dddd, MMMM Do YYYY , h:mm a"));
 $("currentTime").text(moment().format("h:mm a"));
 
-//Compare current time and row time and adjust tense as necessary
+//Load data from local storage
+$("#hour09 .description").val(localStorage.getItem("hour09"));
+$("#hour10 .description").val(localStorage.getItem("hour10"));
+$("#hour11 .description").val(localStorage.getItem("hour11"));
+$("#hour12 .description").val(localStorage.getItem("hour12"));
+$("#hour13 .description").val(localStorage.getItem("hour13"));
+$("#hour14 .description").val(localStorage.getItem("hour14"));
+$("#hour15 .description").val(localStorage.getItem("hour15"));
+$("#hour16 .description").val(localStorage.getItem("hour16"));
+$("#hour17 .description").val(localStorage.getItem("hour17"));
 
+//Compare current time and row time and adjust tense as necessary
 $(".time-block").each(function () {
     var presentTime = moment().hour();
     var blockTime = parseInt($(this).attr("id").split("time")[1]);
@@ -17,14 +27,14 @@ $(".time-block").each(function () {
     else {
       $(this).removeClass("past", "present").addClass("future");
     }
-  });
-
-
-  //Load data from local storage
-
+  })
 
 //Save data to local storage when button clicked
-
+$(".saveBtn").on("click", function () {
+    console.log(this);
+    var text = $(this).siblings(".description").val();
+    var time = $(this).parent().attr("id");
+ })
 
 
 
