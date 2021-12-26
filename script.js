dcf2d514c51dfb59d6f998747962fd0fc8e5f426
Erin H <e.hatherell@gmail.com>
2021-12-26 22:06:47 +1100

    Still debugging, still can't get colours to change

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
 $(document).ready(function() {
-//Display the current date and time
-$("#currentDay").text(moment().format("dddd, MMMM Do YYYY , h:mm a"));
-$("currentTime").text(moment().format("h:mm a"));
+  //Display the current date and time
+  $("#currentDay").text(moment().format("dddd, MMMM Do YYYY , h:mm a"));
+  $("currentTime").text(moment().format("h:mm a"));
 
-//Load data from local storage
-$("#hour09 .description").val(localStorage.getItem("hour09"));
-$("#hour10 .description").val(localStorage.getItem("hour10"));
-$("#hour11 .description").val(localStorage.getItem("hour11"));
-$("#hour12 .description").val(localStorage.getItem("hour12"));
-$("#hour13 .description").val(localStorage.getItem("hour13"));
-$("#hour14 .description").val(localStorage.getItem("hour14"));
-$("#hour15 .description").val(localStorage.getItem("hour15"));
-$("#hour16 .description").val(localStorage.getItem("hour16"));
-$("#hour17 .description").val(localStorage.getItem("hour17"));
+  // Assign saveButtonn click listener 
+  $(".saveButton").on("click", function () {
+  console.log(this);
+  var text = $(this).siblings(".description").val();
+  var time = $(this).parent().attr("id");
+ 
+  // Set items in local storage
+  localStorage.setItem(time, text);
+})
 
-//Compare current time and row time and adjust tense as necessary
-$(".time-block").each(function () {
+  //Load current data from local storage
+  $("#hour09 .description").val(localStorage.getItem("hour09"));
+  $("#hour10 .description").val(localStorage.getItem("hour10"));
+  $("#hour11 .description").val(localStorage.getItem("hour11"));
+  $("#hour12 .description").val(localStorage.getItem("hour12"));
+  $("#hour13 .description").val(localStorage.getItem("hour13"));
+  $("#hour14 .description").val(localStorage.getItem("hour14"));
+  $("#hour15 .description").val(localStorage.getItem("hour15"));
+  $("#hour16 .description").val(localStorage.getItem("hour16"));
+  $("#hour17 .description").val(localStorage.getItem("hour17"));
+
+  //Compare current time and row time and adjust tense as necessary
+  $(".time-block").each(function () {
     var presentTime = moment().hour();
     var blockTime = parseInt($(this).attr("id").split("time")[1]);
     if (blockTime < presentTime) {
