f4ad36dfe8a864a9fe051a580da697680c4711e8
Erin H <e.hatherell@gmail.com>
2021-12-26 22:51:53 +1100

    Debugging and reordering code so it now works

--- script.js
+++ script.js
@@ -2,15 +2,12 @@ $(document).ready(function() {
 
   //Display the current date and time
   $("#currentDay").text(moment().format("dddd, MMMM Do YYYY , h:mm a"));
-  $("currentTime").text(moment().format("h:mm a"));
-
-
+ 
   //Save data to local storage when button clicked
   $(".saveButton").on("click", function () {
   console.log(this);
   var text = $(this).siblings(".description").val();
   var time = $(this).parent().attr("id");
-
 
   // Set items in local storage
   localStorage.setItem(time, text);
@@ -31,26 +28,28 @@ $(document).ready(function() {
 
   //Overall function to change block colours according to time
   function ChangeColour() {
-    var today = new Date();
-    var currentHour= moment().hour();
-    var currentTime = today.getHours();
- 
-    //Compare current time and row time and adjust tense as necessary
-    $(".time-block").each(function () {
-      var presentTime = moment().hour();
-      var blockTime = parseInt($(this).attr("id").split("time")[1]);
-      if (blockTime < presentTime) {
+     $(".time-block").each(function () {
+      var presentTime= moment().hour();
+      var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+      //Compare row times and current time and adjust tense
+      if(blockTime < presentTime) {
         $(this).addClass("past");
-      } 
-      else if (blockTime == presentTime) {
-        $(this).removeClass("past").addClass("present");
-      } 
+        $(this).removeClass("future");
+        $(this).removeClass("present");
+      }
+      else if (blockTime === presentTime) {
+        $(this).removeClass("past");
+        $(this).addClass("present");
+        $(this).removeClass("future");
+      }
       else {
-        $(this).removeClass("past", "present").addClass("future");
+        $(this).removeClass("present");
+        $(this).removeClass("past");
+        $(this).addClass("future");
       }
     })
   }
   ChangeColour();
  
-
 })
